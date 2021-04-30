bdc06a7c57a67cd923778d9c61b1e47e4d911457
derspanischgamer <robertogc99@hotmail.com>
2021-04-30 20:25:43 +0200

    Fixed AY38910RegWrite-library and added Midi receiver test

--- AY3910RegWrite-library/AY3910RegWrite-library.ino
+++ AY3910RegWrite-library/AY3910RegWrite-library.ino
@@ -1,15 +1,18 @@
+#include "Amadeus.h"
+
 // ----------- Variables -----------
 
 char regs[3];
 
+Amadeus amadeus = Amadeus(); // Iniciar la clase Amadeus
+
 // ----------- Funciones -----------
 
 void setup() {
-  Serial.begin(115200);
+  amadeus.begin();
 }
 
 void loop() {
   while (Serial.readBytes(regs, 3) != 3); // Primer byte es el chip, segundo byte es el registro y el tercero es el valor
-  for (int i = 0; i < 3; i++)
-    Serial.write(regs[i] + 1);
+    amadeus.out(regs[0], regs[1], regs[2]); // Escribir en el chip regs[0], en el registro regs[1] el valor [2]
 }
